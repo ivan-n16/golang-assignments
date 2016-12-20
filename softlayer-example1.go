c8703948bc791291caa3e0dc24b771ff4650251f
ivan-n16 <ivan@us.ibm.com>
2016-12-20 12:18:59 +0530

    Added Grab

--- softlayer-example1.go
+++ softlayer-example1.go
@@ -4,6 +4,7 @@ import (
         "fmt"
         "github.com/softlayer/softlayer-go/services"
         "github.com/softlayer/softlayer-go/session"
+        "github.com/softlayer/softlayer-go/sl"
 )
 
 func main() {
@@ -15,11 +16,12 @@ func main() {
         vms, _ := service.Mask("id,hostname,domain,primaryIpAddress,primaryBackendIpAddress,accountId,billingItem[orderItem[order[userRecord[username]]]]").GetVirtualGuests()
 
         for _, vm := range vms {
+/*
                 var primaryIpAddress = ""
                 if vm.PrimaryIpAddress != nil {
                         primaryIpAddress = *vm.PrimaryIpAddress
                 }
-
-                fmt.Printf("ID: %d, Hostname: %s, Domain: %s, PublicIP: %s, PrivateIP: %s, AccountId: %d, Owner: %s\n", *vm.Id, *vm.Hostname, *vm.Domain, primaryIpAddress, *vm.PrimaryBackendIpAddress, *vm.AccountId, *vm.BillingItem.OrderItem.Order.UserRecord.Username)
+*/
+                fmt.Printf("ID: %d, Hostname: %s, Domain: %s, PublicIP: %s, PrivateIP: %s, AccountId: %d, Owner: %s\n", *vm.Id, *vm.Hostname, *vm.Domain, sl.Grab(vm, "PrimaryIpAddress"), *vm.PrimaryBackendIpAddress, *vm.AccountId, *vm.BillingItem.OrderItem.Order.UserRecord.Username)
         }
 }
